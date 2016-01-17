c88e77fd5eedd177659b6034c3a71907fb0cb83e
Alejandro Granadillo <slayerfat@gmail.com>
2016-01-17 16:54:32 -0430

    creado seeder de usuario

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = env('USER_NAME');
+        $user->email = env('USER_EMAIL');
+        $user->password = bcrypt(env('USER_PW'));
+
+        $user->save();
+    }
+}
