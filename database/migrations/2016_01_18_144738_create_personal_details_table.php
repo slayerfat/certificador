f741cc66f5dcdca91877b2248a5bd66054e03158
Alejandro Granadillo <slayerfat@gmail.com>
2016-03-09 11:40:37 -0430

    segundo nombre y apellido puede ser nulo

--- database/migrations/2016_01_18_144738_create_personal_details_table.php
+++ database/migrations/2016_01_18_144738_create_personal_details_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreatePersonalDetailsTable extends Migration
 {
@@ -17,9 +17,9 @@ class CreatePersonalDetailsTable extends Migration
             $table->unsignedInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('first_name', 20);
-            $table->string('last_name', 20);
+            $table->string('last_name', 20)->nullable();
             $table->string('first_surname', 20);
-            $table->string('last_surname', 20);
+            $table->string('last_surname', 20)->nullable();
             $table->unsignedInteger('ci');
             $table->string('phone');
             $table->string('cellphone');
