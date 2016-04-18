5555db68b432e730154449f9929540a7edbeff89
Alejandro Granadillo <slayerfat@gmail.com>
2016-04-18 17:00:18 -0430

    :cry: los titulos deben ir antes de eventos en seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
-        $this->call(EventTableSeeder::class);
         $this->call(TitleTableSeeder::class);
+        $this->call(EventTableSeeder::class);
     }
 }
