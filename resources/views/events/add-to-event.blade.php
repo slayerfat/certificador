7ad7f42034492bd6691db1e747cbfa8218bc4908
Alejandro Granadillo <slayerfat@gmail.com>
2016-04-19 11:30:36 -0430

    admin puede ver mensaje de participacion en evento

--- resources/views/events/add-to-event.blade.php
+++ resources/views/events/add-to-event.blade.php
@@ -1,5 +1,26 @@
 <hr>
 
+@if (Auth::user()->personalDetails && !Auth::user()->personalDetails->events()->where('id', $event->id)->first())
+  <a href="{{ route('events.createAttendantFromSelf', $event->id) }}"
+     class="btn btn-default">
+    <i class="fa fa-btn fa-plus"></i>Solicitar Participación
+  </a>
+@else
+  @if (Auth::user()->personalDetails->events()->where('id', $event->id)->first()->pivot->approved)
+    <h2 class="text-success">Ud. esta participando en este evento.</h2>
+    <a
+      class="btn btn-default"
+      href="{{ route('events.showPdf', [Auth::user()->personalDetails->id, $event->id]) }}"
+      title="Ver Certificado">
+      <i class="fa fa-btn fa-file-pdf-o"></i>Generar Certificado
+    </a>
+  @else
+    <h2 class="text-warning">
+      Ud. esta participando en este evento, pero no ha sido aun aprobado.
+    </h2>
+  @endif
+@endif
+
 @if (Auth::user()->admin)
   <a href="{{ route('events.createProfessors', $event->id) }}"
      class="btn btn-default">
@@ -10,25 +31,4 @@
      class="btn btn-default">
     <i class="fa fa-btn fa-plus"></i>Asignar Participantes
   </a>
-@else
-  @if (Auth::user()->personalDetails && !Auth::user()->personalDetails->events()->where('id', $event->id)->first())
-    <a href="{{ route('events.createAttendantFromSelf', $event->id) }}"
-       class="btn btn-default">
-      <i class="fa fa-btn fa-plus"></i>Solicitar Participación
-    </a>
-  @else
-    @if (Auth::user()->personalDetails->events()->where('id', $event->id)->first()->pivot->approved)
-      <h2 class="text-success">Ud. esta participando en este evento.</h2>
-      <a
-        class="btn btn-default"
-        href="{{ route('events.showPdf', [Auth::user()->personalDetails->id, $event->id]) }}"
-        title="Ver Certificado">
-        <i class="fa fa-btn fa-file-pdf-o"></i>Generar Certificado
-      </a>
-    @else
-      <h2 class="text-warning">
-        Ud. esta participando en este evento, pero no ha sido aun aprobado.
-      </h2>
-    @endif
-  @endif
 @endif
