12d4484db56b2a3afa15e7faf66c588a624672aa
Alejandro Granadillo <slayerfat@gmail.com>
2016-09-28 13:07:58 -0400

    fix(frontend): register button to full block size

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -101,7 +101,7 @@
 
               <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
-                  <button type="submit" class="btn btn-primary">
+                  <button type="submit" class="btn btn-primary  btn-block">
                     <i class="fa fa-btn fa-user"></i>Registrarse
                   </button>
 
