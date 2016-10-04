e19c45717ad73564a8a2285b53ddf7cb78659fe9
Alejandro Granadillo <slayerfat@gmail.com>
2016-10-04 15:02:23 -0400

    style: update email form to spanish

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -4,6 +4,6 @@ return [
     "password" => "La clave debe ser al menos seis caracteres y debe igualar la confirmación.",
     "user"     => "No se puede encontrar un usuario asociado al correo electrónico.",
     "token"    => "Este link de cambio de clave es invalido.",
-    "sent"     => "Le hemos enviado un enlace de cambio de clave!",
+    "sent"     => "Le hemos enviado un enlace de cambio de clave, este correo puede tardar varios minutos en llegar.",
     "reset"    => "Su clave ha sido cambiada!",
 ];
